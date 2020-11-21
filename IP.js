8d1fc46ae1d73fb301ebfc0468dc302c3bcd6157
afiffudin-au <afiffudin077@gmail.com>
2020-11-21 20:13:56 +0700

    add regex to check ip and create validation function

--- IP.js
+++ IP.js
@@ -3,9 +3,12 @@ const inputSubnet = document.querySelector('.inputSubnet')
 const calculateIp = document.querySelector('.calculate')
 const calculateResult = document.querySelector('.calculateResult')
 const calculation = document.querySelector('.calculation')
-const calculation_text = document.querySelectorAll('.calculation_text')
+const calculation_texts = document.querySelectorAll('.calculation_text')
 const desc = document.querySelector('.desc')
 const timeExecution = document.querySelector('.timeExecution')
+const beforeCalculations = document.querySelectorAll('.beforeCalculation')
+const erorrMesseage = document.querySelector('.erorrMesseage')
+const regex = '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
 let valueIp = ''
 let valueSubnet = ''
 const ClassIP = {
@@ -59,37 +62,39 @@ function calculate(ip, prefix) {
       <p class="note">Note : Cara perhitungan dibawah diambil dari proses perhitungan dibalik layar </p>`
     )
     desc.innerHTML = (content)
-    calculation_text.forEach(e => {
+    calculation_texts.forEach(e => {
       e.style.padding = '5px'
     })
-    calculation_text[0].innerHTML = (`<td>Calculate IP</td> <td>${selectArray} / ${ClassIP[`__${prefix}`]} = ${Math.floor(selectArray / ClassIP[`__${prefix}`])} * ${ClassIP[`__${prefix}`]} = ${Math.floor(selectArray / ClassIP[`__${prefix}`]) * ClassIP[`__${prefix}`]}</td>`)
+    beforeCalculations[0].innerHTML = (`Prefix ${prefix} = ${ClassIP[`__${prefix}`]} IP`)
+    beforeCalculations[1].innerHTML = (`4th Octet = ${afterSplit[3]}`)
+    calculation_texts[0].innerHTML = (`<td>Calculate IP</td> <td>${selectArray} / ${ClassIP[`__${prefix}`]} = ${Math.floor(selectArray / ClassIP[`__${prefix}`])} * ${ClassIP[`__${prefix}`]} = ${Math.floor(selectArray / ClassIP[`__${prefix}`]) * ClassIP[`__${prefix}`]}</td>`)
     const firstIP = Math.floor(selectArray / ClassIP[`__${prefix}`]) * ClassIP[`__${prefix}`]
     const lastIP = firstIP + ClassIP[`__${prefix}`] - 1
     const copyArray = [...afterSplit]
     const copyArray2 = [...afterSplit]
     copyArray[3] = firstIP
     firstIPArray = (copyArray)
-    calculation_text[1].innerHTML = (`<td>IP firstResult</td> <td>${afterSplit.join('.')} 
+    calculation_texts[1].innerHTML = (`<td>IP firstResult</td> <td>${afterSplit.join('.')} 
     => ${firstIPArray.join('.')} </td>`)
-    calculation_text[2].innerHTML = (`<td>Calculate IP</td>  <td>${firstIP} + ${ClassIP[`__${prefix}`]} - 1 = ${firstIP + ClassIP[`__${prefix}`] - 1}</td>`)
+    calculation_texts[2].innerHTML = (`<td>Calculate IP</td>  <td>${firstIP} + ${ClassIP[`__${prefix}`]} - 1 = ${firstIP + ClassIP[`__${prefix}`] - 1}</td>`)
     copyArray2[3] = lastIP
     lastIPArray = (copyArray2)
-    calculation_text[3].innerHTML = (`<td>IP lastResult</td> <td>${afterSplit.join('.')} 
+    calculation_texts[3].innerHTML = (`<td>IP lastResult</td> <td>${afterSplit.join('.')} 
     => ${lastIPArray.join('.')} </td>`)
-    calculation_text[4].innerHTML = (`<td>NETWORK ID (firstResult)</td><td>${firstIPArray.join('.')}</td>`)
-    calculation_text[5].innerHTML = (`<td>BROADCAST (lastResult)</td><td>${lastIPArray.join('.')}</td>`)
+    calculation_texts[4].innerHTML = (`<td>NETWORK ID (firstResult)</td><td>${firstIPArray.join('.')}</td>`)
+    calculation_texts[5].innerHTML = (`<td>BROADCAST (lastResult)</td><td>${lastIPArray.join('.')}</td>`)
     subnetMask = 256 - ClassIP[`__${prefix}`]
-    calculation_text[6].innerHTML = (`<td>Subnet calculate</td> <td>256 - ${ClassIP[`__${prefix}`]} = ${256 - ClassIP[`__${prefix}`]}</td>`)
-    calculation_text[7].innerHTML = (`<td>Subnet result</td> <td>255.255.255.${subnetMask}</td>`)
+    calculation_texts[6].innerHTML = (`<td>Subnet calculate</td> <td>256 - ${ClassIP[`__${prefix}`]} = ${256 - ClassIP[`__${prefix}`]}</td>`)
+    calculation_texts[7].innerHTML = (`<td>Subnet result</td> <td>255.255.255.${subnetMask}</td>`)
     const copyArray3 = [...firstIPArray]
     copyArray3[3] = copyArray3[3] + 1
     usableHostIpRange.first = copyArray3
     const copyArray4 = [...lastIPArray]
     copyArray4[3] = copyArray4[3] - 1
     usableHostIpRange.last = copyArray4
-    calculation_text[8].innerHTML = (`<td>RANGE IP </td> <td>${firstIPArray.join('.')} - ${lastIPArray.join('.')}</td>`)
-    calculation_text[9].innerHTML = (`<td>VALID HOST Range</td><td>${usableHostIpRange.first.join('.')} - ${usableHostIpRange.last.join('.')}</td>`)
-    calculation_text[10].innerHTML = (`<td>Valid Host</td> <td>${(ClassIP[`__${prefix}`])} - 2 = ${(ClassIP[`__${prefix}`] - 2)}</td>`)
+    calculation_texts[8].innerHTML = (`<td>RANGE IP </td> <td>${firstIPArray.join('.')} - ${lastIPArray.join('.')}</td>`)
+    calculation_texts[9].innerHTML = (`<td>VALID HOST Range</td><td>${usableHostIpRange.first.join('.')} - ${usableHostIpRange.last.join('.')}</td>`)
+    calculation_texts[10].innerHTML = (`<td>Valid Host</td> <td>${(ClassIP[`__${prefix}`])} - 2 = ${(ClassIP[`__${prefix}`] - 2)}</td>`)
     const results = (
       `
       <h3>Hasil</h3>
@@ -147,43 +152,45 @@ function calculate(ip, prefix) {
     )
     desc.innerHTML = (content)
     desc.innerHTML = (content)
-    calculation_text.forEach(e => {
+    calculation_texts.forEach(e => {
       e.style.padding = '5px'
     })
     prefixInt = parseInt(prefix)
     const imaginer = parseInt(prefixInt + 8)
-    calculation_text[0].innerHTML = (`<td>imaginer</td><td>${prefix} + 8 = ${prefixInt + 8}</td>`)
+    beforeCalculations[0].innerHTML = (`Imaginer Prefix ${prefix} = ${imaginer} = ${ClassIP[`__${imaginer}`]} IP`)
+    beforeCalculations[1].innerHTML = (`3rd Octet = ${afterSplit[2]}`)
+    calculation_texts[0].innerHTML = (`<td>imaginer</td><td>${prefix} + 8 = ${prefixInt + 8}</td>`)
     const IPclass = ClassIP[`__${imaginer}`]
     const totalIP = IPclass * 256
-    calculation_text[1].innerHTML = (`<td>total IP </td> <td>  ${IPclass} * 256 = ${IPclass * 256}</td>`)
+    calculation_texts[1].innerHTML = (`<td>total IP </td> <td>  ${IPclass} * 256 = ${IPclass * 256}</td>`)
     const firstIP = Math.floor(selectArray2 / IPclass) * IPclass
-    calculation_text[2].innerHTML = (`<td>Calculate IP </td> <td> ${selectArray2} / ${IPclass} = ${Math.floor(selectArray2 / IPclass)} * ${IPclass} = ${Math.floor(selectArray2 / IPclass) * IPclass}</td>`)
+    calculation_texts[2].innerHTML = (`<td>Calculate IP </td> <td> ${selectArray2} / ${IPclass} = ${Math.floor(selectArray2 / IPclass)} * ${IPclass} = ${Math.floor(selectArray2 / IPclass) * IPclass}</td>`)
     const lastIP = firstIP + IPclass - 1
     const copyArray = [...afterSplit]
     copyArray[3] = 0
     copyArray[2] = firstIP
     firstIPArray = copyArray
-    calculation_text[3].innerHTML = (`<td>IP firstResult</td> <td>${afterSplit.join('.')} => ${firstIPArray.join('.')}</td>`)
-    calculation_text[4].innerHTML = (`<td>Calculate IP</td> <td>${firstIP} + ${IPclass} - 1 = ${firstIP + IPclass - 1}</td>`)
+    calculation_texts[3].innerHTML = (`<td>IP firstResult</td> <td>${afterSplit.join('.')} => ${firstIPArray.join('.')}</td>`)
+    calculation_texts[4].innerHTML = (`<td>Calculate IP</td> <td>${firstIP} + ${IPclass} - 1 = ${firstIP + IPclass - 1}</td>`)
     const copyArray2 = [...afterSplit]
     copyArray2[3] = 255
     copyArray2[2] = lastIP
     lastIPArray = copyArray2
-    calculation_text[5].innerHTML = (`<td>IP lastResult</td> <td>${afterSplit.join('.')} => ${lastIPArray.join('.')}</td>`)
-    calculation_text[6].innerHTML = (`<td>NETWORK ID (firstResult) </td> <td>${firstIPArray.join('.')}</td>`)
-    calculation_text[7].innerHTML = (`<td>BROADCAST (lastResult)</td>  <td>${lastIPArray.join('.')}</td>`)
+    calculation_texts[5].innerHTML = (`<td>IP lastResult</td> <td>${afterSplit.join('.')} => ${lastIPArray.join('.')}</td>`)
+    calculation_texts[6].innerHTML = (`<td>NETWORK ID (firstResult) </td> <td>${firstIPArray.join('.')}</td>`)
+    calculation_texts[7].innerHTML = (`<td>BROADCAST (lastResult)</td>  <td>${lastIPArray.join('.')}</td>`)
     subnetMask = 256 - IPclass
-    calculation_text[8].innerHTML = (`<td>Subnet calculate</td>  <td>256 - ${IPclass} = ${256 - IPclass}</td>`)
-    calculation_text[9].innerHTML = (`<td>SUBNET MASK </td> <td>255.255.${subnetMask}.0</td>`)
+    calculation_texts[8].innerHTML = (`<td>Subnet calculate</td>  <td>256 - ${IPclass} = ${256 - IPclass}</td>`)
+    calculation_texts[9].innerHTML = (`<td>SUBNET MASK </td> <td>255.255.${subnetMask}.0</td>`)
     const copyArray3 = [...firstIPArray]
     copyArray3[3] = copyArray3[3] + 1
     usableHostIpRange.first = copyArray3
     const copyArray4 = [...lastIPArray]
     copyArray4[3] = copyArray4[3] - 1
     usableHostIpRange.last = copyArray4
-    calculation_text[10].innerHTML = (`<td>RANGE IP </td> <td>${firstIPArray.join('.')} - ${lastIPArray.join('.')}</td>`)
-    calculation_text[11].innerHTML = (`<td>VALID HOST Range</td> <td>${usableHostIpRange.first.join('.')} - ${usableHostIpRange.last.join('.')}</td>`)
-    calculation_text[12].innerHTML = (`<td>Valid Host </td> <td>${(totalIP)} - 2 = ${(totalIP - 2)}</td>`)
+    calculation_texts[10].innerHTML = (`<td>RANGE IP </td> <td>${firstIPArray.join('.')} - ${lastIPArray.join('.')}</td>`)
+    calculation_texts[11].innerHTML = (`<td>VALID HOST Range</td> <td>${usableHostIpRange.first.join('.')} - ${usableHostIpRange.last.join('.')}</td>`)
+    calculation_texts[12].innerHTML = (`<td>Valid Host </td> <td>${(totalIP)} - 2 = ${(totalIP - 2)}</td>`)
     const results = (
       `
     <h3>Hasil</h3>
@@ -233,40 +240,39 @@ function calculate(ip, prefix) {
     timeExecution.innerHTML = `Execution time: ${Math.floor(end - start)} ms`
   }
 }
+function validation(valueIp){
+  beforeCalculations.forEach(e=>{
+    e.innerHTML = ''
+  })
+  calculation_texts.forEach(e => {
+    e.innerHTML = ''
+  })
+  calculateResult.innerHTML = ''
+  desc.innerHTML = ''
+  console.log("bresih bersih")
+  if(valueIp.match(regex)===null){
+    return true
+  }else{
+    erorrMesseage.innerHTML = ''
+    return false
+  }
+}
 function handleInput(e) {
   valueIp = e.target.value
-  if (valueIp === '' || valueSubnet === '') {
-    calculation_text.forEach(e => {
-      e.innerHTML = ''
-      e.style.padding = '0'
-    })
-    calculateResult.innerHTML = ''
-    desc.innerHTML = ''
+  if(validation(valueIp) === true){
     return
   }
   calculate(valueIp, valueSubnet)
 }
 function handleInputSubnet(e) {
   valueSubnet = e.target.value
-  if (valueIp === '' || valueSubnet === '') {
-    calculation_text.forEach(e => {
-      e.innerHTML = ''
-      e.style.padding = '0'
-    })
-    calculateResult.innerHTML = ''
-    desc.innerHTML = ''
+  if(validation(valueIp,valueSubnet) === true){
     return
   }
   calculate(valueIp, valueSubnet)
 }
 function handleCalculate() {
-  if (valueIp === '' || valueSubnet === '') {
-    calculation_text.forEach(e => {
-      e.innerHTML = ''
-      e.style.padding = '0'
-    })
-    calculateResult.innerHTML = ''
-    desc.innerHTML = ''
+  if(validation(valueIp,valueSubnet) === true){
     return
   }
   calculate(valueIp, valueSubnet)
